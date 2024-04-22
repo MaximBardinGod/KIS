9b78e451896d681e389bc2b44d7cdebb137096a6
Бардин Максим Сергеевич <105451157+MaximBardinGod@users.noreply.github.com>
2024-04-22 20:09:57 +0300

    create db conection

---
+++ KIS/dbConfig.js
@@ -0,0 +1,20 @@
+const sql = require('msnodesqlv8');
+
+const connectionString = "server=KOMPUTER\\SQLEXPRESS;Database=KIS;trusted_connection=yes;driver={SQL Server Native Client 11.0}";
+
+function connectToDatabase() {
+    return new Promise((resolve, reject) => {
+        sql.open(connectionString, (err, conn) => {
+            if (err) {
+                console.error('Не удалось подключится к БД! Ошбика:', err);
+                return reject(err);
+            }
+            console.log('Подключение к БД...');
+            resolve(conn);
+        });
+    });
+}
+
+module.exports = {
+    connectToDatabase
+};
